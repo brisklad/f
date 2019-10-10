031326cb688d6e957c0a81756b01b3a09afbfac9
kodgirl <startsava@gmail.com>
2019-10-10 17:26:19 +0300

    half with lists

--- fillit.c
+++ fillit.c
@@ -6,13 +6,24 @@
 /*   By: bjasper <bjasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 16:45:13 by bjasper           #+#    #+#             */
-/*   Updated: 2019/10/09 21:31:01 by bjasper          ###   ########.fr       */
+/*   Updated: 2019/10/10 17:25:16 by bjasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "libft/libft.h"
+#include "/Users/bjasper/bjasper/libft/libft.h"
 #include "fillit.h"
+#include <fcntl.h>
+
+void	ft_save(char *buf, t_list *tetromino)
+{
+	
+	if (tetromino->content == NULL)
+		tetromino->content = ft_strdup(buf);
+	else
+		tetromino->next = ft_lstnew(buf, 21);
+	printf("%s", tetromino->content);
+}
 
 int		ft_cmp(int a, int *mass)
 {
@@ -55,46 +66,46 @@ int		ft_tetrovalid(int *p)
 	return (0);
 }
 
-int		ft_skip(char *s, int i)
+int		ft_check_simbol(char *s)
 {
-	if (s[i] == '\n' && (i + 1) % 5 == 0)
+	int		*sharp;
+	int		i;
+	int		j;
+	t_list	*tetromino;
+
+	i = 0;
+	j = 0;
+	sharp = (int *)malloc(sizeof(int) * 4);
+	tetromino = ft_lstnew(NULL, 21);
+	while (i < 20)
 	{
-		++i;
-		return (1);
+		if (s[i] == '#' && (i + 1) % 5 != 0 && j < 4)
+			sharp[j++] = i++ + 1;
+		else if (s[i] == '\n' && (i + 1) % 5 == 0)
+			++i;
+		else if (s[i] == '.' && (i + 1) % 5 != 0)
+			++i;
+		else
+			return (0);
 	}
-	else if (s[i] == '.' && (i + 1) % 5 != 0)
-	{
-		++i;
-		return (1);
-	}
-	return (0);
+	if ((!ft_tetrovalid(sharp)) || j != 4)
+		return (0);
+	ft_save(s, tetromino);
+	int a = ft_list_size(tetromino);
+	printf("%d\n", a );
+	return (1);
 }
 
 int		ft_strvalid(int fd, char *buf)
-{
-	int		i;
-	int		j;
-	int		*sharp;
+{	
 	int		red;
 
 	while ((red = read(fd, buf, BUFF_SIZE)))
 	{
-		i = 0;
-		j = 0;
-		sharp = (int *)malloc(sizeof(int) * 4);
 		if (red != 21 || (buf[20] != '\n' && buf[20] != '\0'))
 			return (0);
-		while (i < 20)
-		{
-			if (buf[i] == '#' && (i + 1) % 5 != 0 && j < 4)
-				sharp[j++] = i++ + 1;
-			else if (ft_skip(buf, i) == 1)
-				++i;
-			else
-				return (0);
-		}
-		if ((!ft_tetrovalid(sharp)) || j != 4)
-			return (0);
+		if (ft_check_simbol(buf) == 0)
+			return (0);		
 	}
 	return (1);
 }
